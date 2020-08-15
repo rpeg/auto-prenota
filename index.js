b4167bb28b2da25c3b7de2707abfdc851e1a913e
Sean Johnson <sean.johnson.ca@gmail.com>
2020-08-15 13:43:50 -0700

    fixes + safety

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-console */
 const forever = require('forever-monitor');
+const shell = require('shelljs');
 
 const child = new (forever.Monitor)('monitor.js', {
   silent: false,
   killTree: true,
-  args: [],
 });
 
 child.on('exit:code', (code) => {
@@ -12,8 +12,13 @@ child.on('exit:code', (code) => {
   child.restart();
 });
 
+child.on('start', () => {
+  console.info('Target script start');
+});
+
 child.on('restart', () => {
   console.info('Target script restarted');
+  shell.exec('pkill chrome'); // prune zombie browser processes
 });
 
 child.on('error', () => {
